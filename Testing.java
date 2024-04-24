eef5f12117958df81c17570cb140ad15a21634a4
Dhara <160832703+DharaHirpara@users.noreply.github.com>
2024-04-24 18:35:45 +0200

    Create Testing.java

---
+++ Testing.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class Testing {
+
+    int i=10;
+    private By name= By.id("test");
+
+    public void getName(){
+
+        System.out.println("Remote master");
+
+    }
+}
